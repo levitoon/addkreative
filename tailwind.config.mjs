131d0b118efceb46599e64cb0a72f57bcbd783f1
Levian Lara <levitoon@gmail.com>
2025-01-15 15:42:31 +0100

    feat:  Using tailwind and setting Layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+/*export default {
+	content: ['./src/!**!/!*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+	},
+	plugins: [],
+}*/
+
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			fontFamily: {
+				'ubuntu': ['Ubuntu', 'sans-serif'],
+				'noto': ['Noto Sans', 'sans-serif'],
+			},
+			colors: {
+				night: '#00140dff',
+				darkGreen: '#00311eff',
+				seaGreen: '#1c875eff',
+				silver: '#bec2c5ff',
+				davysGray: '#504f54ff',
+				darkerGray: '#333333',
+				lightGray: '#a0a0a0',
+			},
+		},
 	},
 	plugins: [],
 }
